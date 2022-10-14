b9c882e05fae69c9071ee33e0fe51e60e7464a30
Timilehin Jegede <timilehintimothy.jegede@gmail.com>
2022-10-14 02:13:10 +0100

    initial: open the SKY.

---
+++ lib/intro_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: Container()),
+          Expanded(
+            child: Column(
+              children: [],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
